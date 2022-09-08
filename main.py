d2acf03a2769d4793e27dbf0426de3f1d37b5840
zhouzi <zijun06>
2022-09-08 16:14:21 -0400

    comite initial

---
+++ main.py
@@ -0,0 +1,9 @@
+#Entrant:
+#Une chaîne de caractère qui contient seulement des mots en minuscules ainsi que des espaces. Peut contenir des mots de la langue française, mais aussi des onomatopées.
+
+#Sortant:
+#Vous devez afficher dans la console le nombre de mot exact.
+
+chaine = str(input("Entre le la chane de carectere que vous voulez calculer: "))
+print("vous avez ", len(chaine.split(" ")), "carectere dasn cette chain.")
+
